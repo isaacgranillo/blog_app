7e97ccdf605ec8813ffce6722fcedcb5a8f4cb47
Isaac Granillo <isaac.granillo@gmail.com>
2015-12-23 12:19:18 -0800

    updated article controller and added error messages

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ group :production do
 	gem 'rails_12factor'
 end
 
+gem 'hirb'
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get "/about", to: "pages#about"
 
+  resources :articles
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
